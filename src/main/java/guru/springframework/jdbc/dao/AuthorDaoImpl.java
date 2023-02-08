44871cfa6c5445151f9e53332731202c7a36caf1
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2023-02-08 18:06:03 +0200

    replaced statement with prepared statement

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 @RequiredArgsConstructor
 @Component
@@ -20,17 +20,19 @@ public class AuthorDaoImpl implements AuthorDao {
     public Author getById(Long id) {
         try (
                 Connection connection = source.getConnection();
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM author WHERE id=" + id);
+                PreparedStatement ps = connection.prepareStatement("SELECT * FROM author WHERE id=?");
         ) {
-            if (resultSet.next()) {
-                return Author.builder()
-                        .id(id)
-                        .firstName(resultSet.getString("first_name"))
-                        .lastName(resultSet.getString("last_name"))
-                        .build();
-            }
+            ps.setLong(1, id);
 
+            try (ResultSet resultSet = ps.executeQuery()) {
+                if (resultSet.next()) {
+                    return Author.builder()
+                            .id(id)
+                            .firstName(resultSet.getString("first_name"))
+                            .lastName(resultSet.getString("last_name"))
+                            .build();
+                }
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
