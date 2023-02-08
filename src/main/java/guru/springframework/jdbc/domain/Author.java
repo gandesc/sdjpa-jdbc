86147ebd5180d06254f16f728035196906658e2f
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2023-02-08 08:29:32 +0200

    completed asignment

---
+++ src/main/java/guru/springframework/jdbc/domain/Author.java
@@ -0,0 +1,21 @@
+package guru.springframework.jdbc.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Author {
+
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
